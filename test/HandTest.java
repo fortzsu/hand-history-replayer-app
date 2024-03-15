22ca2267d0b8c5c230263c1fceafff150e610a73
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-03-15 17:00:28 +0100

    add PositionGeneratorTest

--- test/HandTest.java
+++ test/HandTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.NoSuchElementException;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HandTest {
