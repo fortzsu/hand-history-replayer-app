c9aed5cf3ae7b8efb66deccc6820695af25887ed
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-03-17 20:11:30 +0100

    refact tests

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -5,13 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
-    @Test
-    public void testPlayerActionsSetter() {
-        Player player = new Player();
-        String originalAction = "Fayemouse: folds ";
-        player.setAction(originalAction);
-        Assertions.assertEquals("folds ", player.getActions().get(0));
-    }
+
 
     @Test
     public void testSetChipsInBigBlind_withNoChip_fail() {
@@ -34,14 +28,6 @@ public class PlayerTest {
         Assertions.assertEquals("0,22", player.getChipsInBigBlind());
     }
 
-    @Test
-    public void testAddActions_ok() {
-        String originalAction_one = "Fayemouse: folds ";
-        String originalAction_two = "Fayemouse: posts the ante 3";
-        Player player = new Player();
-        player.setAction(originalAction_one);
-        player.setAction(originalAction_two);
-        Assertions.assertEquals(1, player.getActions().size());
-    }
+
 
 }
